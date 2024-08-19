be2ca5bd9949e093c832889dacfd11a5de2565cc
Tanh_Chan <119776880+TrTueTah@users.noreply.github.com>
2024-08-19 22:23:49 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const fontSans = FontSans({
   variable: "--font-sans",
 });
 
-export const metadata: Metadata = {
+const metadata: Metadata = {
   title: "LiveDocs",
   description: "Your go-to collaborative editor",
 };
